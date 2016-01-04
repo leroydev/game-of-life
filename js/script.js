e973011a928b5c1f50eaa0120a6b34ff4f0eff4a
leroydev <leroydev@taw.net>
2016-01-04 16:31:10 +0100

    Added the actual project files

---
+++ js/script.js
@@ -0,0 +1,149 @@
+var civilization = [
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0],
+  [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+var i,
+    j,
+    rectSize = 20,
+    interval = 100;
+
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d");
+ctx.canvas.height = rectSize * civilization.length;
+ctx.canvas.width = rectSize * civilization[0].length;
+
+function numberOfNeighbours(x, y) {
+  var nrOfNeighbours = 0;
+
+  //If cell above given cell is alive
+  if (y !== 0) {
+    if (civilization[y-1][x] === 1) {
+      nrOfNeighbours++;
+    }
+  }
+
+  //If cell left from given cell is alive
+  if (x !== 0) {
+    if (civilization[y][x-1] === 1) {
+      nrOfNeighbours++;
+    }
+  }
+
+  //If cell below given cell is alive
+  if (y !== civilization.length-1) {
+    if (civilization[y+1][x] === 1) {
+      nrOfNeighbours++;
+    }
+  }
+
+  //If cell right from given cell is alive
+  if (x !== civilization[0].length-1) {
+    if (civilization[y][x+1] === 1) {
+      nrOfNeighbours++;
+    }
+  }
+
+  //If cell above and left of given cell is alive
+  if (x !== 0 && y !== 0) {
+    if (civilization[y-1][x-1] === 1) {
+      nrOfNeighbours++;
+    }
+  }
+
+  //If cell below and left of given cell if alive
+  if (x !== 0 && y !== civilization.length-1) {
+    if (civilization[y+1][x-1] === 1) {
+      nrOfNeighbours++;
+    }
+  }
+
+  //If cell above and right of given cell is alive
+  if (x !== civilization[0].length-1 && y !== 0) {
+    if (civilization[y-1][x+1] === 1) {
+      nrOfNeighbours++;
+    }
+  }
+
+  //If cell below and right of given cell is alive
+  if (x !== civilization[0].length-1 && y !== civilization.length-1) {
+    if (civilization[y+1][x+1] === 1) {
+      nrOfNeighbours++;
+    }
+  }
+
+  return nrOfNeighbours;
+}
+
+function playForGod() {
+  var futureCivilization = [];
+  for (y = 0; y < civilization.length; y++) {
+    futureCivilization[y] = civilization[y].slice(0);
+    for (x = 0; x < civilization[y].length; x++) {
+      var nrOfNeighbours = numberOfNeighbours(x, y);
+
+      if (civilization[y][x] === 0) {
+        //Any dead cell with exactly three live neighbours becomes a live cell
+        if (nrOfNeighbours === 3) {
+          futureCivilization[y][x] = 1;
+        }
+      } else if (civilization[y][x] === 1) {
+        //Any live cell with more than three live neighbours dies
+        if (nrOfNeighbours > 3) {
+          futureCivilization[y][x] = 0;
+        //Any live cell with fewer than two live neighbours dies
+        } else if (nrOfNeighbours < 2) {
+          futureCivilization[y][x] = 0;
+        }
+      }
+    }
+  }
+
+  for (y = 0; y < futureCivilization.length; y++) {
+    civilization[y] = futureCivilization[y].slice(0);
+  }
+  drawCivilization();
+}
+
+function drawCivilization() {
+  for (y = 0; y < civilization.length; y++) {
+    for (x = 0; x < civilization[y].length; x++) {
+      if (civilization[y][x] === 1) {
+        ctx.fillRect(x * rectSize, y * rectSize, rectSize, rectSize);
+      } else {
+        ctx.clearRect(x * rectSize, y * rectSize, rectSize, rectSize);
+      }
+    }
+  }
+}
+
+drawCivilization();
+playForGod();
+window.setInterval(playForGod, interval);
